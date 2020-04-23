bf546c5181a3c4314cf49d17aa45c614a96d359b
Yves Desjardins <45479020+YvesDesjardins@users.noreply.github.com>
2020-04-23 15:25:58 -0700

    add oscillating obstacles

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour {
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;
+    [Range(0, 1)] [SerializeField] float movementFactor; // 0 = not moved, 1 = fully moved
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start() {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float cycles = Time.time / period; // protect against divides by 0
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f; // go from -1 to +1
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
